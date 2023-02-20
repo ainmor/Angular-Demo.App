eec5aec88c37b2dc5e8975dfba6cfa4bcde827e2
Ain Mor <ainbmor@gmail.com>
2023-02-20 17:57:41 +0100

    implement api in based on the service

---
+++ src/app/modules/product/product.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../services/product.service";
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  constructor(
+    private productService: ProductService
+  ) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe( (products) => {
+      console.log(products);
+    }, (error) => {
+      console.error(error);
+    });
+  }
+}
